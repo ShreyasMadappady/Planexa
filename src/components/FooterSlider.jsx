2dcab75772c823f8cdecd9607af6db5c68fa6e2b
Shreyas M S <shreyasmadappady20@gmail.com>
2024-03-21 11:34:28 +0530

    Commit

--- src/components/FooterSlider.jsx
+++ src/components/FooterSlider.jsx
@@ -1,15 +1,6 @@
 import { motion } from "framer-motion";
 
-// Define the array of slides with numbers
-const slides = [
-  { number: "FOOTBALL" },
-
-  { number: "BASKETBALL" },
-
-  { number: "ICEHOCKEY" },
-
-  { number: "BASKETBALL" },
-];
+const slides = ["FOOTBALL ", "BASKETBALL", "ICEHOCKEY", "BASKETBALL"];
 function FooterSlider() {
   const duplicatedSlides = [...slides, ...slides];
   return (
@@ -33,15 +24,14 @@ function FooterSlider() {
           },
         }}
       >
-        {/* Render duplicated slides */}
         {duplicatedSlides.map((slide, index) => (
           <div
             key={index}
-            className="flex-shrink-0"
+            className="flex-shrink-0 "
             style={{ width: `${100 / slides.length}%` }}
           >
-            <div className="bg-black  items-center justify-center     text-[#FFF1E1] flex  w-full  py-[1.12rem] text-xs mb-[2.90rem] font-medium ">
-              {slide.number}
+            <div className="bg-black  items-center justify-center   text-[#FFF1E1] flex   py-[1.12rem] text-xs mb-[2.90rem] font-medium ">
+              {slide}
             </div>
           </div>
         ))}
