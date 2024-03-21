f4a63d7866aa9e7f81995f590bcfac328cba3932
Shreyas M S <shreyasmadappady20@gmail.com>
2024-03-21 10:45:31 +0530

    Commit

--- src/components/CentreImage.jsx
+++ src/components/CentreImage.jsx
@@ -1,10 +1,9 @@
 function CentreImage() {
   return (
-    <div
-      className={`bg-[url('src/images/Image7.png')] bg-cover bg-opacity-40`}
-    >
+    <div className="bg-[url('src/images/Image7.png')] bg-cover bg-opacity-40">
       <h1 className="text-2xl font-semibold text-white pl-[2.21rem] py-[2.29rem] pr-[5.13rem]">
-        At Planexa, we believe in the transformative power of community engagement and the vital role of wellness in our lives.
+        At Planexa, we believe in the transformative power of community
+        engagement and the vital role of wellness in our lives.
       </h1>
     </div>
   );
