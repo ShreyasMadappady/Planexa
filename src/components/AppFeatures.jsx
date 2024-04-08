799a4e812cddcd8efbdc037907c692fab783a660
Shreyas M S <shreyasmadappady20@gmail.com>
2024-04-08 11:41:18 +0530

    Commit

--- src/components/AppFeatures.jsx
+++ src/components/AppFeatures.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 function AppFeatures() {
   return (
     <div className="bg-[url('/src/images/Image10.webp')] text-white flex flex-col items-center font-medium bg-cover py-[3.17rem]  lg:pt-[8.188rem] lg:pb-[6rem]">
@@ -6,17 +7,153 @@ function AppFeatures() {
       </h1>
       <div className="lg:flex lg:gap-[9rem]">
         <div className="">
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
         </div>
         <div className="">
           {" "}
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
-          <h1 className="text-[2rem]  pb-[2.5rem]">1. ABC one</h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              // if odd index card,slide from right instead of left
+              y: 50,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0, // Slide in to its original position
+              transition: {
+                duration: 1, // Animation duration
+              },
+            }}
+            viewport={{ once: true }}
+            className="text-[2rem]  pb-[2.5rem]"
+          >
+            1. ABC one
+          </motion.h1>
         </div>
       </div>
     </div>
